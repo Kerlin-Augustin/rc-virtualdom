64069e282e1775d7a30fa3dac433b4267dbec966
Kerlin Augustin <kerlin.augustin1@gmail.com>
2024-04-01 20:06:57 -0400

    counting every three seconds

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,11 @@ import { useState } from "react";
 function App() {
   const [count, setCount] = useState(0);
 
-  const increment = () => {
-    setCount(count + 1);
-  };
-
   setInterval(() => {
-    increment(), 3000;
-  });
+    setCount(count + 1);
+  }, 3000);
 
-  return <h1>Count every 3 seconds{count}</h1>;
+  return <h1>Count every 3 seconds {count}</h1>;
 }
 
 export default App;
